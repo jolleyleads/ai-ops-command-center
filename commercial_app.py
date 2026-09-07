dee695a9d31d88c285de093ac3c14ae03e16251e
Matthew Jolley <jolleyleads@gmail.com>
2026-09-07 00:47:25 -0400

    Register verified job signal endpoint

--- commercial_app.py
+++ commercial_app.py
@@ -2,3 +2,4 @@ from universal_app import app
 import prospect_engine  # noqa: F401
 import places_diagnostic  # noqa: F401
 import permit_leads  # noqa: F401
+import verified_job_signals  # noqa: F401
