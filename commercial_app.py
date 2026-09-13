b3cb54328e29dafd57e1f488856a18768aab23e3
Matthew Jolley <jolleyleads@gmail.com>
2026-09-13 05:10:49 -0400

    Register local job search route

--- commercial_app.py
+++ commercial_app.py
@@ -3,3 +3,4 @@ import prospect_engine  # noqa: F401
 import places_diagnostic  # noqa: F401
 import permit_leads  # noqa: F401
 import verified_job_signals  # noqa: F401
+import local_jobs  # noqa: F401
