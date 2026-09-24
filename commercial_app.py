b00cd1d0fdde53c4465373eb2b4005cd60ae1cf0
Matthew Jolley <jolleyleads@gmail.com>
2026-09-24 15:09:08 -0400

    Load Gmail reconciliation hardening after outreach automation

--- commercial_app.py
+++ commercial_app.py
@@ -9,6 +9,7 @@ import local_jobs  # noqa: F401
 import rag_research  # noqa: F401
 import smart_search  # noqa: F401
 import outreach_automation  # noqa: F401
+import gmail_reconciliation  # noqa: F401
 import smart_search_outreach_hook  # noqa: F401
 import outreach_scheduler  # noqa: F401
 import db_diagnostic  # noqa: F401
